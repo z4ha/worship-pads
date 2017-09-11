5cb691e1edddcfb5cf2b8905460d5cc859ace293
z4ha <jakmusze@gmail.com>
2017-09-12 00:34:20 +0200

    Add jQuery toggle for playing note

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 var sound = new Howl({
   src: ['./assets/sounds/c.mp3']
 });
+var playChecker;
+var fadeTime = 1000;
 
 $('.c').click(function() {
-  if (sound.playing() && sound.volume() === 1) {
-    sound.fade(1.0, 0, 1000);
-    setTimeout(function() { sound.stop()}, 1000);
+  if (sound.playing() && playChecker === true) {
+    playChecker = false;
+    sound.fade(1.0, 0, fadeTime);
+    setTimeout(function() { sound.stop(); }, fadeTime);
   } 
   if (sound.playing() == false) {
     sound.play();
+    sound.fade(0, 1.0, fadeTime);
+    playChecker = true;
   }
+  $( this ).toggleClass( "highlight" );
 });
+
